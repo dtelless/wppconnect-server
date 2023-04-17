25a9e77785db95cb58554060ae9769840fdf2806
Daniel Telles <dtelless@gmail.com>
2023-04-17 14:35:39 -0300

    fix: Arrumando conexao Redis

--- src/util/db/redis/db.ts
+++ src/util/db/redis/db.ts
@@ -5,10 +5,11 @@ const redis = config.tokenStoreType === 'redis' ? require('redis') : null;
 let RedisClient: any = null;
 
 if (config.tokenStoreType === 'redis') {
-  RedisClient = redis.createClient(config.db.redisPort, config.db.redisHost, {
-    password: config.db.redisPassword,
-    db: config.db.redisDb,
+  RedisClient = redis.createClient({
+    url: config.db.redisHost,
+    legacyMode: true,
   });
+  RedisClient.connect();
 }
 
 export default RedisClient;
